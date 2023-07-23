760b672088320ef18fa6c357795eef3811cafef8
oladejomubarak <oladejomubarakade@gmail.com>
2023-07-24 00:43:15 +0100

    first commit

---
+++ src/main/java/oladejo/mubarak/niquestore/NiquestoreApplication.java
@@ -0,0 +1,13 @@
+package oladejo.mubarak.niquestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiquestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiquestoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oladejo/mubarak/niquestore/NiquestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package oladejo.mubarak.niquestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiquestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
