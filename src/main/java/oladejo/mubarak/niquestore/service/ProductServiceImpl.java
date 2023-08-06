6a32d9bf52b995b46052ae59bcac6cbaf3a8cccf
oladejomubarak <oladejomubarakade@gmail.com>
2023-08-06 09:18:24 +0100

    adding product services

--- src/main/java/oladejo/mubarak/niquestore/service/ProductServiceImpl.java
+++ src/main/java/oladejo/mubarak/niquestore/service/ProductServiceImpl.java
@@ -3,17 +3,34 @@ package oladejo.mubarak.niquestore.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import oladejo.mubarak.niquestore.data.dto.request.ProductDto;
+import oladejo.mubarak.niquestore.data.model.AppUser;
 import oladejo.mubarak.niquestore.data.model.Product;
+import oladejo.mubarak.niquestore.data.model.Role;
+import oladejo.mubarak.niquestore.exception.NiqueStoreException;
+import oladejo.mubarak.niquestore.repository.ProductRepo;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class ProductServiceImpl implements ProductService{
+    private final UserServiceImpl userService;
+    private final ProductRepo productRepo;
+
     @Override
     public Product addProduct(ProductDto productDto) {
-        return null;
+        AppUser foundUser = userService.findByEmail(productDto.getVendorEmail());
+        if(!foundUser.getRole().contains(Role.VENDOR)) {throw new NiqueStoreException("Only vendors can add product");
+        }
+        Product product = new Product();
+        product.setName(productDto.getProductName());
+        product.setCategory(productDto.getCategory());
+        product.setPrice(BigDecimal.valueOf(productDto.getPrice()));
+        product.setQuantity(productDto.getQuantity());
+        product.setStoreAddress(productDto.getStoreAddress());
+        return productRepo.save(product);
     }
 
     @Override
