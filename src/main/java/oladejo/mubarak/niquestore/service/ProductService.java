322625d97a882b10dc5effb60402c33b21bcb5c1
oladejomubarak <oladejomubarakade@gmail.com>
2023-08-06 04:06:11 +0100

    adding product services

--- src/main/java/oladejo/mubarak/niquestore/service/ProductService.java
+++ src/main/java/oladejo/mubarak/niquestore/service/ProductService.java
@@ -4,8 +4,14 @@ package oladejo.mubarak.niquestore.service;
 import oladejo.mubarak.niquestore.data.dto.request.ProductDto;
 import oladejo.mubarak.niquestore.data.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
     Product addProduct(ProductDto productDto);
     Product findProduct(String productId);
     Product editProduct(String productId, ProductDto productDto);
+    String deleteProduct(String productId);
+    List<Product> searchProductByName(String productName);
+    List<Product> searchProductByCategory(String category);
+
 }
