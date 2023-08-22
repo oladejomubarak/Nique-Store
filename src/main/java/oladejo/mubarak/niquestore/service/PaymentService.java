9f4b54eb47caa6d3cbab7b5248806b9669db3a68
oladejomubarak <oladejomubarakade@gmail.com>
2023-08-22 16:15:07 +0100

    adding order services

--- src/main/java/oladejo/mubarak/niquestore/service/PaymentService.java
+++ src/main/java/oladejo/mubarak/niquestore/service/PaymentService.java
@@ -1,5 +1,10 @@
 package oladejo.mubarak.niquestore.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class PaymentService {
 private final String pay_stack_key = System.getenv("PAY_STACK_SECRET_KEY");
 }
