488838e381259f9aec80940c63f094f4d63522a4
oladejomubarak <oladejomubarakade@gmail.com>
2023-09-18 21:30:41 +0100

    implementing tests cases

--- src/test/java/oladejo/mubarak/niquestore/service/OrderServiceImplTest.java
+++ src/test/java/oladejo/mubarak/niquestore/service/OrderServiceImplTest.java
@@ -1,8 +1,14 @@
 package oladejo.mubarak.niquestore.service;
 
+import oladejo.mubarak.niquestore.data.dto.request.OrderProductRequest;
+import oladejo.mubarak.niquestore.data.model.Order;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -10,7 +16,22 @@ class OrderServiceImplTest {
     @Autowired
     private OrderServiceImpl orderService;
 
+    private OrderProductRequest orderProductRequest;
+
     @BeforeEach
     void setUp() {
+        orderProductRequest = new OrderProductRequest();
+        orderProductRequest.setCustomerEmail("oladejomubarakade@gmail.com");
+        orderProductRequest.setProductId("64efa68aa1b41925d2d4aebc");
+        orderProductRequest.setQuantity(2);
+    }
+     @Test
+     void testThatProductCanBeOrdered(){
+        Order order = orderService.orderProduct(orderProductRequest);
+        assertEquals(BigDecimal.valueOf(10000.0), order.getTotalPrice());
+    }
+    @Test void testThatOrderCanBeFound(){
+        Order order = orderService.findOrder("6508b0cf5a11e20ec877dceb");
+        assertEquals(BigDecimal.valueOf(10000.0), order.getTotalPrice());
     }
 }
