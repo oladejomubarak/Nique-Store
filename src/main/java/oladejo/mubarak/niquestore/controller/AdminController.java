9aefe4487cd79a3ca5897785a97da6412bd0051e
oladejomubarak <oladejomubarakade@gmail.com>
2023-12-13 16:34:05 +0100

    adjusting controller

--- src/main/java/oladejo/mubarak/niquestore/controller/AdminController.java
+++ src/main/java/oladejo/mubarak/niquestore/controller/AdminController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1/admin/")
+@RequestMapping("/api/v1/admin/")
 @RequiredArgsConstructor
 public class AdminController {
 
--- src/main/java/oladejo/mubarak/niquestore/controller/UserController.java
+++ src/main/java/oladejo/mubarak/niquestore/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1/auth/")
+@RequestMapping("/api/v1/auth/")
 @RequiredArgsConstructor
 @CrossOrigin("*")
 public class UserController {
